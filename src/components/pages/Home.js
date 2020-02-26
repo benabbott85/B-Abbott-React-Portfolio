9fb91db1508a6d3e24fab62d98f00561cba0085b
Ben Abbott <babbott85@gmail.com>
2020-02-26 09:58:34 -0700

    started to work on home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -4,17 +4,13 @@ import React from "react";
 function Home() {
   return (
     <div>
-      <h1>Home Page</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque velit, lobortis ut magna
-        varius, blandit rhoncus sem. Morbi lacinia nisi ac dui fermentum, sed luctus urna tincidunt.
-        Etiam ut feugiat ex. Cras non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna
-        imperdiet ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras rutrum
-        ligula in tincidunt commodo. Morbi sit amet mollis orci, in tristique ex. Donec nec ornare
-        elit. Donec blandit est sed risus feugiat porttitor. Vestibulum molestie hendrerit massa non
-        consequat. Vestibulum vitae lorem tortor. In elementum ultricies tempus. Interdum et
-        malesuada fames ac ante ipsum primis in faucibus.
-      </p>
+      <h1 className="myname">Ben Abbott</h1>
+      <br></br>
+      <h2 className="fullstack">Full Stack Web Developer</h2>
+    <br></br>
+      <h3 className="denver"> Denver, Colorado</h3>
+      <br></br>
+     
     </div>
   );
 }
