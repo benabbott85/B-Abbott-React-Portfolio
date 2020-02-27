133acac256379c9c62801490ed301a8393b30511
Ben Abbott <babbott85@gmail.com>
2020-02-27 09:27:26 -0700

    linkedin and github added to homepage

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -10,8 +10,12 @@ function Home() {
     <br></br>
       <h3 className="denver"> Denver, Colorado</h3>
       <br></br>
-     
+     <a href="https://www.linkedin.com/in/ben-abbott-51394421/"><i class="fab fa-linkedin fa-4x linkedin" ></i></a>
+      <br></br>
+      <a href="https://github.com/benabbott85"><i class="fab fa-github fa-4x github"></i></a>
     </div>
+
+
   );
 }
 
